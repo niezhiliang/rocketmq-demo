3a697fadf1d437207ceecd570b2f8da8e1e85eb4
“niezl@hzzh.com” <niezl@hzzh.com>
2021-01-04 17:28:16 +0800

    整合springBoot

--- my-rocketmq/src/main/java/cn/isuyu/my/rocketmq/Application.java
+++ my-rocketmq/src/main/java/cn/isuyu/my/rocketmq/Application.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -14,6 +15,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @date : 2021/1/4
  */
 @SpringBootApplication
+@RestController
 public class Application {
     @Autowired
     private DefaultMQProducer defaultMQProducer;
